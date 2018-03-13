2292902c30954a72c1bf0e90b120b78a2e1d4ede
Abhishek Piedy <abhishek.piedy@gmail.com>
2018-03-13 17:18:48 -0400

    Add Gifts Model and routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,14 +3,16 @@ const express = require('express'),
       dataService = require('../services/dataService'),
       userService = require('../services/userService'),
       mailerService = require('../services/mailerService'),
-      codeService = require('../services/codeService');
+      giftService = require('../services/codeService');
 
 router.get('/data', dataService.getData);
 router.post('/data', dataService.saveData);
 router.get('/users', userService.getUsers);
 router.post('/users', userService.saveUser);
 router.post('/send', mailerService.sendGridMail);
-router.post('/giftcode', codeService.saveCode);
-router.get('/giftcode', codeService.getCodes);
+router.post('/giftcode', giftService.saveCode);
+router.get('/giftcode', giftService.getCodes);
+router.post('/gift', giftService.assignGift);
+router.get('/gift', giftService.getGifts);
 
 module.exports = router;
--- server/services/giftService.js
+++ server/services/giftService.js
@@ -1,4 +1,5 @@
 let Code = require('../db/models/giftCodes');
+let Gift = require('../db/models/gifts');
 
 module.exports.saveCode = function(req, res) {
     let codes = new Code();
@@ -29,5 +30,37 @@ module.exports.getCodes = function(req, res) {
             res.status(500).send({"msg": "Internal Server Error"});
         }
         res.status(200).send({"giftCodes": codes});
+    })
+}
+
+module.exports.getGifts = function(req, res) {
+    Gift.find(function(err, gifts) {
+        if(err) {
+            console.log(err);
+            res.status(500).send({"msg": "Internal Server Error"});
+        }
+        res.status(200).send({"gifts": gifts})
+    })
+}
+
+module.exports.assignGift = function(req, res) {
+    let gifts = new Gift();
+
+    gifts.userId = req.body.uid;
+    gifts.assignedGift = req.body.gift;
+
+    gifts.save(function(err) {
+        if(err) {
+            if(err.name === 'BulkWriteError' && err.code === 11000)
+            {
+                return res.status(409).send({"msg": "Existing"})
+            }
+            else {
+                console.log(err.message);
+                return res.status(500).send({"msg": "Internal Server Error"})
+            }
+        } else {
+            res.status(200).send({"msg":"Code Saved!"});
+        }
     })
 }
